5b807d52534cec50df818fb6bab4ca3a770f32d6
Bruce <bruce.fan0308@gmail.com>
2024-11-06 11:51:11 -0500

    [test]

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'dtqeyrmtlalnznxwyief.supabase.co'
+                hostname: 'nrihfivdrsxovdvfnyqm.supabase.co'
             }
         ]
     }
